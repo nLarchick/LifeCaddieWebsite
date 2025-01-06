0026145e56e7e312e06451b86af2cf628a37c5ae
Noah <larchicknoah@gmail.com>
2025-01-05 19:00:19 -0700

    Finished banner, simple body section.

--- desktop/d_home.php
+++ desktop/d_home.php
@@ -29,20 +29,35 @@
             </div>
             <div class="bannerR">
                 <button class="bannerButton">First Button</button>
-                <script>
-                    let bannerSizing = document.querySelector(".banner");
-                    let bannerHeight = bannerSizing.offsetHeight;
-                    let brElement = document.createElement("br");
-                    brElement.style.height = `${bannerHeight/8}px`;
-                    bannerSizing.appendChild(brElement);
-                </script>
-                <br style="height: 5px;">
+                <br>
                 <button class="bannerButton">-- Need to be this wide. --</button>
             </div>
         </div>
     </div>
-    <div class="temp">
-        <p>This is a lil test!</p>
+    <div style="height: 2.5vw;"></div>
+    <div class="aboutSection">
+        <h3>About Us</h3>
+        <h5>Learn more about the Life Caddie company</h5>
+        <div style="height: 2.5vw;"></div>
+        <div class="aboutContent">
+            <div class="aboutL">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi varius hendrerit libero, a varius dolor 
+                    sodales quis. Maecenas facilisis lectus dictum erat pretium eleifend. Ut cursus ante diam, non vestibulum 
+                    urna semper sed. Nunc ac maximus erat, non tristique metus. Donec nec metus et mauris gravida vestibulum. 
+                    Integer et mi nec nibh ultricies pretium quis ac sapien. Vestibulum id leo lacus. In finibus ut odio vel 
+                    ullamcorper. Curabitur at neque tempor dui posuere pharetra. Vivamus fringilla, orci a accumsan vulputate, 
+                    est est porta dui, eu feugiat nisl est at velit. Sed finibus sodales ante sodales mattis. Nullam gravida 
+                    purus ac ante elementum accumsan. Etiam congue orci viverra augue convallis, id vestibulum lectus 
+                    sollicitudin. Vestibulum bibendum urna sit amet dictum faucibus. Duis ligula ex, consectetur eu risus 
+                    nec, semper pellentesque lorem. Vivamus vulputate a dolor eget pretium.
+                </p>
+            </div>
+            <div class="aboutR">
+                <img src="\LifeCaddieWebsite\assets\pictures\LifeCaddieLogo.jpg" alt="Life Caddie Logo." width="100%" />
+            </div>
+        </div>
+        <div style="height: 2.5vw;"></div>
     </div>
 </body>
 </html>
