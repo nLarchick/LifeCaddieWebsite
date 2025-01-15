c396f83ad89706b4e262b4a4bddfc2c7634b717a
Noah <larchicknoah@gmail.com>
2025-01-14 18:05:24 -0700

    Added all secondary pages for desktop version.

---
+++ desktop/d_contact.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * d_contact.php
+ *
+ * This is the desktop version contact us page.
+ *
+ * @author Noah Larchick <larchicknoah@gmail.com>
+ * @copyright Copyright (c) 2024 Life Caddie
+ * @license <MIT>
+ */
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- link rel="stylesheet" href="../assets/css/desktop_style.css" -->
+    <title>Contact Us</title>
+</head>
+<body>
+    <h1>CONTACT PAGE</h1>
+    <p>This is the inital structure for the contact us page.</p>
+</body>
+</html>
---
+++ desktop/d_services.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * d_services.php
+ *
+ * This is the desktop version services page.
+ *
+ * @author Noah Larchick <larchicknoah@gmail.com>
+ * @copyright Copyright (c) 2024 Life Caddie
+ * @license <MIT>
+ */
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- link rel="stylesheet" href="../assets/css/desktop_style.css" -->
+    <title>Services</title>
+</head>
+<body>
+    <h1>SERVICES PAGE</h1>
+    <p>This is the inital structure for the services page.</p>
+</body>
+</html>
