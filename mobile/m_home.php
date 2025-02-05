dde5ff5660b3d2c0d07dabb552013aec1c6c7f9e
Noah <larchicknoah@gmail.com>
2025-02-05 09:47:05 -0700

    mobile version re-work.

--- mobile/m_home.php
+++ mobile/m_home.php
@@ -17,50 +17,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../assets/css/mobile_style.css">
-    <script src="../assets/js/menuConvert.js"></script>
     <title>Home</title>
 </head>
-<header>
-    <div class="navMenuButton" onclick="openMenu()">
-        <div class="line"></div>
-        <div class="line"></div>
-        <div class="line"></div>
-        <div class="line"></div>
-    </div>
-    <div class="menu">
-        <button class="navButton">First button!</button>
-        <button class="navButton">Second button!</button>
-        <button class="navButton">Third button!</button>
-        <button class="navButton">Fourth button!</button>
-    </div>
-</header>
-<body>
-<div class="banner">
-        <img src="../assets/pictures/CleanHouse.png" alt="Banner photo of clean home." width="100%" />
-        <div class="bannerInternal">
-            <h2>We can help with Downsizing</h2>
-            <button class="bannerButton">First Button</button>
-            <button class="bannerButton">-- Need to be this wide. --</button>
-        </div>
-</div>
-<div style="height: 2.5vw;"></div>
-<div class="aboutSection">
-        <div class="aboutContent">
-            <h3>About Us</h3>
-            <h5>Learn more about the Life Caddie company</h5>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi varius hendrerit libero, a varius dolor 
-                sodales quis. Maecenas facilisis lectus dictum erat pretium eleifend. Ut cursus ante diam, non vestibulum 
-                urna semper sed. Nunc ac maximus erat, non tristique metus. Donec nec metus et mauris gravida vestibulum. 
-                Integer et mi nec nibh ultricies pretium quis ac sapien. Vestibulum id leo lacus. In finibus ut odio vel 
-                ullamcorper. Curabitur at neque tempor dui posuere pharetra.
-            </p>
-            <img src="../assets/pictures/LifeCaddieLogo.jpg" alt="Life Caddie Logo." width="100%" />
-        </div>
-</div>
-<div style="height: 2.5vw;"></div>
-<footer>
-    <?php include 'm_footer.php'; ?>
-</footer>
-</body>
 </html>
